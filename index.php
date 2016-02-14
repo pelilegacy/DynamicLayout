626210bc844e1804902c770df5eca45be25ef631
Niko Heikkilä <niko.j.heikkila@student.jyu.fi>
2016-02-14 22:01:41 +0200

    Move CSS definitions to own file

--- index.php
+++ index.php
@@ -3,67 +3,7 @@
 <head>
 	<title>Layout</title>
 	<meta charset="utf-8">
-    <style>
-
-		/* Font for texts */
-	    @font-face {
-	        font-family: ChunkFive;
-	        src: url("ChunkFive.otf");
-	    }
-
-	    body {
-	        background-image: url("pelilegacy-layout-2015-kesa-169.png");
-	        background-repeat: no-repeat;
-	    }
-
-	    #game, #name {
-	        display: block;
-	        font-family: ChunkFive, sans-serif;
-	        text-align: center;
-	        color: white;
-	        font-size: 36px;
-	        position: absolute;
-	    }
-
-	    #name {
-	        width: 440px;
-	        bottom: 0px;
-	        right: 0px;
-	    }
-
-	    #game {
-	        width: 840px;
-	        top: 14px;
-	        left: 400px;
-	    }
-
-        #alert {
-	        width: 439px;
-            height: 701px;
-	        top: 66px;
-	        left: 1481px;
-            border: 0px;
-            position: absolute;
-	    }
-
-        #artbox {
-	        width: 194px;
-            height: 662px;
-	        top: 85px;
-	        left: 1706px;
-            border: 0px solid green;
-            position: absolute;
-	    }
-
-        #pic {
-            position: relative;
-            top: 50%;
-            transform: translateY(-50%);
-            width: 99%;
-            border: 2px solid;
-            border-radius: 25px;
-        }
-    </style>
+	<link rel="stylesheet" href="layout.css" media="all" charset="utf-8">
 </head>
 <body>
 	<div id="game"></div>
